2c1654f1b412da0939213c737e34ddd0701228de
Irchik512 <chornairchik@gmail.com>
2023-04-25 19:38:06 +0300

    StyledHomeView + Provider

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import { store, persistor} from 'redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-       <BrowserRouter basename="/goit-react-hw-08-phonebook">
-        <App />
-       </BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,7 +1,16 @@
+import { Section } from "components/Section/Section.styled";
+import { Heading, ButtonGroup, Button } from '@chakra-ui/react'
+import { Link } from "react-router-dom";
+
 export default function HomeView() {
   return(
-    <>
-      <h1>Home View</h1>
-    </>
+    <Section>
+      <Heading as='h1' size='3xl' p={'90px 0'} >
+        Create your own 'web' PhoneBook!
+      </Heading>        
+      <ButtonGroup gap='4'>
+      <Button colorScheme='blackAlpha'><Link to='/register'>Registration</Link></Button>
+      <Button colorScheme='blackAlpha'><Link to='/login'>Login</Link></Button>
+    </ButtonGroup></Section>
   )
 };
